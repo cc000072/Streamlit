44db29513ebb3638a02e65cf172b4c7a967822ef
cc000072 <43445858+cc000072@users.noreply.github.com>
2022-10-02 17:34:02 +0900

    Add files via upload

---
+++ Proof_Of_Concept.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+print("Start...")
+
+try:
+
+    st.title("hello")
+
+except Exception as e:
+
+    print(e)
+
+finally:
+
+    pass
+
+print("Finish...")
+
+
+# In[ ]:
+
+
+
+
